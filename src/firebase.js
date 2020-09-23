62b27e9815651a97471df0980afcc16cf4528424
soumyadeepsp <soumyadeepsp@gmail.com>
2020-09-24 00:40:50 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD6rTr3sZUN2cfEib8KBdc5oh5hB5aoOBI",
+    authDomain: "messenger-clone-92f21.firebaseapp.com",
+    databaseURL: "https://messenger-clone-92f21.firebaseio.com",
+    projectId: "messenger-clone-92f21",
+    storageBucket: "messenger-clone-92f21.appspot.com",
+    messagingSenderId: "1066544436980",
+    appId: "1:1066544436980:web:b76b05b149dcff55dc70a1"
+};
